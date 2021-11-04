d7cb002d0c0501fd58a5daca3c1db099f192a4b8
Dmytro <dimaniko04@ukr.net>
2021-11-04 16:19:05 +0200

    Downloaded and connected the Merriweather font. Created basic header, footer and navigation bar with styles. Added a new reset mixin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.scss";
+
+const NavBar = () => {
+  return (
+    <ul className={styles.navBar}>
+      <li>Каталог</li>
+      <li>Доставка</li>
+      <li>Оплата</li>
+      <li>Контакты</li>
+      <li>О галерее</li>
+    </ul>
+  );
+};
 
 function App() {
   return (
-    <div className="app">
-      <img src="img/museumLogo1.svg" alt="logo" />
-      <h1>Hello World!</h1>
-      <img src="img/museumLogo2.svg" alt="logo" />
+    <div className={styles.app}>
+      <header>
+        <img src="img/museumLogo1.svg" alt="Logo" />
+        <NavBar />
+      </header>
+      <main>
+        <h1 className={styles.regular}>Hello World!</h1>
+        <h1 className={styles.bold}>Hello World!</h1>
+        <h1 className={styles.light}>Hello World!</h1>
+      </main>
+      <footer>
+        <img src="img/museumLogo2.svg" alt="Logo" />
+        <NavBar />
+      </footer>
     </div>
   );
 }
