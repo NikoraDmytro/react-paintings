c31197f40322c4d27d9c195f3f4ecfd4dc0be464
Dmytro <dimaniko04@ukr.net>
2021-11-06 12:39:34 +0200

    Added search bar in header and contact information in footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,13 +2,25 @@ import React from "react";
 
 import { NavBar } from "../NavBar/index";
 
-import "./Footer.module.scss";
+import styles from "./Footer.module.scss";
 
 export const Footer = () => {
   return (
     <footer>
       <img src="img/museumLogo2.svg" alt="Logo" />
       <NavBar />
+
+      <div className={styles.contacts}>
+        <div>
+          <img className={styles.icon} src="img/phoneIcon.svg" alt="address" />
+          <span>+380 (99) 999-99-99</span>
+        </div>
+
+        <div className={styles.address}>
+          <img className={styles.icon} src="img/addressIcon.svg" alt="phone" />
+          <span>г. Харьков, ул. Академика Павлова., 24</span>
+        </div>
+      </div>
     </footer>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavBar } from "../NavBar/index";
 
-import "./Header.module.scss";
+import styles from "./Header.module.scss";
 
 export const Header = () => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <header>
       <img src="./img/museumLogo1.svg" alt="Logo" />
       <NavBar />
+      <div className={styles.searchBar}>
+        <input
+          className={styles.searchField}
+          type="text"
+          value={search}
+          onChange={handleChange}
+          placeholder="Поиск по названию картины"
+        />
+
+        <button>Найти</button>
+      </div>
     </header>
   );
 };
